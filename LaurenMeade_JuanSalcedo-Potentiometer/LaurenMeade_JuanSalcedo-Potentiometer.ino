43dcee5773fad5062c7237f363037849b2da841f
Lauren Meade <lauren.meade@gulllakecs.org>
2019-05-01 12:22:48 -0400

    Addeddate, creators, and purpose.

---
+++ LaurenMeade_JuanSalcedo-Potentiometer/LaurenMeade_JuanSalcedo-Potentiometer.ino
@@ -0,0 +1,18 @@
+/*
+ 5.1.19
+ Creators: Juan Salcedo, Lauren Meade
+ We are designing a program that allows you 
+ to turn a potentionmeter and turns on different LEDS
+ */
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
